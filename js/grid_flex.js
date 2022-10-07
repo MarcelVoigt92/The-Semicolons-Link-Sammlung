5993d7cf53b32d576655181ad69bd903ae196684
TheUnterhemd <kodel.thomas83@gmail.com>
2022-10-07 10:49:18 +0200

    js nun auch mit rows

--- js/grid_flex.js
+++ js/grid_flex.js
@@ -1,19 +1,28 @@
-function showGridCol() {
-  let theValue = document.getElementById("value").value;
-  if (theValue) {
+function showGrid() {
+  let valueCol = document.getElementById("valueCol").value;
+  let valueRow = document.getElementById("valueRow").value;
+  if (valueCol || valueRow) {
     const clearAll = document.getElementById("output");
     clearAll.innerHTML = "";
 
-    let array = theValue.split(" ");
-    console.log(array);
+    let arrayCol = valueCol.split(" ");
+    console.log(arrayCol);
+    let arrayRow = valueRow.split(" ");
+    console.log(arrayRow);
 
-    for (let i = 0; i < array.length; i++) {
+    for (let i = 0; i < arrayCol.length; i++) {
       const createDiv = document.createElement("div");
       createDiv.innerHTML = "das ist eine Spalte!";
       document.getElementById("output").appendChild(createDiv);
     }
+    for (let i = 0; i < arrayRow.length; i++) {
+      const createDiv = document.createElement("div");
+      createDiv.innerHTML = "das ist eine Reihe!";
+      document.getElementById("output").appendChild(createDiv);
+    }
 
     document.getElementById("output").style.display = "grid";
-    document.getElementById("output").style.gridTemplateColumns = theValue;
+    document.getElementById("output").style.gridTemplateColumns = valueCol;
+    document.getElementById("output").style.gridTemplateRows= valueRow;
   }
 }
